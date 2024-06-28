b5c7653fd0420291f437a3bce6ad1dd219659574
Adrián Diaz <diaz.adrian.g@gmail.com>
2024-06-28 13:10:05 +0200

    Fix build

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 project = 'GPCRchimeraDB'
 copyright = '2024, Wim Vranken, Bio2Byte, VUB'
-author = 'Charlotte Crauwels, Adrián Díaz'
+author = 'Wim Vranken, Charlotte Crauwels, Adrián Díaz'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
@@ -36,7 +36,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = "_static/logo_docs.png"
+# html_logo = "_static/logo_docs.png"
 html_static_path = ['_static']
 
 # Set master doc
